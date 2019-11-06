846b8aff4b4b4c1b67cb16f4db4105a74759e8cc
Amanda Campos <amanda.gcampos@amedigital.com>
2019-11-06 17:53:58 -0300

    WIP: post planet in db in progress...

--- src/main/java/br/com/amandacampos/starwarsplanets/StarWarsPlanetsApplication.java
+++ src/main/java/br/com/amandacampos/starwarsplanets/StarWarsPlanetsApplication.java
@@ -3,9 +3,9 @@ package br.com.amandacampos.starwarsplanets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
-@EnableFeignClients
 public class StarWarsPlanetsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StarWarsPlanetsApplication.class, args);
