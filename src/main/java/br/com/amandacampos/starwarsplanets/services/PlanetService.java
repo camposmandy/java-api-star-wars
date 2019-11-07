7334f427a3c056ee55ce077a4be35a085f57f013
Amanda Campos <amanda.gcampos@amedigital.com>
2019-11-07 09:51:06 -0300

    refactor(webflux): list all planets in database with flux

--- src/main/java/br/com/amandacampos/starwarsplanets/services/PlanetService.java
+++ src/main/java/br/com/amandacampos/starwarsplanets/services/PlanetService.java
@@ -2,6 +2,7 @@ package br.com.amandacampos.starwarsplanets.services;
 
 import br.com.amandacampos.starwarsplanets.models.Planet;
 import org.springframework.http.ResponseEntity;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -10,6 +11,6 @@ public interface PlanetService {
     Mono<Planet> save(Planet planet);
 //    ResponseEntity<Planet> findById(Long id);
 //    Planet findByName(String name);
-//    List<Planet> findAll();
+    Flux<Planet> findAll();
 //    ResponseEntity<?> delete(Long id);
 }
